5f35b4a06d11176c1a3195b0515259b4539bfefd
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-12-28 16:38:51 -0500

    style: formatting

--- core/health/main.go
+++ core/health/main.go
@@ -5,24 +5,24 @@ import "os/exec"
 // Check the tools for windows
 // Return: ScoopExists, GitExists, Chocolatey Exists
 func CheckToolsHealthWin() (bool, bool, bool) {
-  _, errScoop := exec.LookPath("scoop")
-  _, errGit := exec.LookPath("git")
-  _, errChoco := exec.LookPath("choco")
-  return errScoop == nil, errGit == nil, errChoco == nil
+	_, errScoop := exec.LookPath("scoop")
+	_, errGit := exec.LookPath("git")
+	_, errChoco := exec.LookPath("choco")
+	return errScoop == nil, errGit == nil, errChoco == nil
 }
 
 // Check the tools for linux
 // Return: GitExists, SnapExists
-func CheckToolsHealthLinux() ( bool, bool ) {
-  _, errGit := exec.LookPath("git")
-  _, errSnap := exec.LookPath("snap")
-  return errGit == nil, errSnap == nil
+func CheckToolsHealthLinux() (bool, bool) {
+	_, errGit := exec.LookPath("git")
+	_, errSnap := exec.LookPath("snap")
+	return errGit == nil, errSnap == nil
 }
 
 // Check the tools for MacOS
 // Return: GitExists, BrewExists
 func CheckToolsHealthMacOs() (bool, bool) {
-  _, errGit := exec.LookPath("git")
-  _, errBrew := exec.LookPath("brew")
-  return errGit == nil, errBrew == nil
+	_, errGit := exec.LookPath("git")
+	_, errBrew := exec.LookPath("brew")
+	return errGit == nil, errBrew == nil
 }
