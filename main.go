78e7f2c74ec818159bb8c95407e2dfe011c4addd
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-12-28 16:38:31 -0500

    feat: improve the health checker for the tools

--- main.go
+++ main.go
@@ -10,42 +10,42 @@ import (
 )
 
 var app = &cli.App{
-  Name: "spider",
-  Version: "1.0.0",
-  Compiled: time.Now(),
-  Authors: []*cli.Author{
-    &cli.Author{
-      Name: "Teo",
-      Email: "malehurtadoreyes@hotmail.com",
-    },
-  },
-  Copyright: "(c) 2021 Licenced by Apache 2.0 Opensource by TeoDev1611",
-  HelpName: "spider",
-  Usage: "The most easy way to install a program - Help: github.com/TeoDev1611/spider",
+	Name:     "spider",
+	Version:  "1.0.0",
+	Compiled: time.Now(),
+	Authors: []*cli.Author{
+		{
+			Name:  "Teo",
+			Email: "malehurtadoreyes@hotmail.com",
+		},
+	},
+	Copyright: "(c) 2021 Licenced by Apache 2.0 Opensource by TeoDev1611",
+	HelpName:  "spider",
+	Usage:     "The most easy way to install a program - Help: github.com/TeoDev1611/spider",
 }
 
-func init(){
-  log.Config.AppName = "spider"
-  log.Config.FileToLog = "spider.log.json"
-  err := log.Init()
-  if err != nil {
-    panic(err)
-  }
+func init() {
+	log.Config.AppName = "spider"
+	log.Config.FileToLog = "spider.log.json"
+	err := log.Init()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-  app.Commands = []*cli.Command{
-    {
-      Name: "init",
-      Aliases: []string{"i", "new"},
-      Usage: "Init a file for download",
-      Description: "Start a new SpiderFile for download the programs",
-    },
-  }
+	app.Commands = []*cli.Command{
+		{
+			Name:        "init",
+			Aliases:     []string{"i", "new"},
+			Usage:       "Init a file for download",
+			Description: "Start a new SpiderFile for download the programs",
+		},
+	}
 
-  err := app.Run(os.Args)
-  if err != nil {
-    string := fmt.Sprintf("Error at start the app complete error here -> %s", err.Error())
-    log.Fatal(string)
-  }
+	err := app.Run(os.Args)
+	if err != nil {
+		string := fmt.Sprintf("Error at start the app complete error here -> %s", err.Error())
+		log.Fatal(string)
+	}
 }
