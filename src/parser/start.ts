47ba94935a816b220feb48ce70e5c37281b01a75
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-01-01 20:05:47 -0500

    feat: improve support for macos and linux

--- src/parser/start.ts
+++ src/parser/start.ts
@@ -1,13 +1,30 @@
 import { toml } from "src/deps.ts";
 import * as log from "utils/logs.ts";
 import os from "https://deno.land/x/dos@v0.11.0/mod.ts";
-import { DEFAULT_DATA_WINDOWS, SPIDER_FILE_PATH } from "parser/default.ts";
+import {
+  DEAFULT_DATA_LINUX,
+  DEFAULT_DATA_MACOS,
+  DEFAULT_DATA_WINDOWS,
+  SPIDER_FILE_PATH,
+} from "parser/default.ts";
 
 export function WriteSpiderFile() {
   const platform: string = os.platform();
   switch (platform) {
     case "windows": {
       const data = toml.stringify(DEFAULT_DATA_WINDOWS);
+      Deno.writeTextFileSync(SPIDER_FILE_PATH, data);
+      log.info("Writed the SpiderFile.toml");
+      break;
+    }
+    case "linux": {
+      const data = toml.stringify(DEAFULT_DATA_LINUX);
+      Deno.writeTextFileSync(SPIDER_FILE_PATH, data);
+      log.info("Writed the SpiderFile.toml");
+      break;
+    }
+    case "darwin": {
+      const data = toml.stringify(DEFAULT_DATA_MACOS);
       Deno.writeTextFileSync(SPIDER_FILE_PATH, data);
       log.info("Writed the Spiderfile.toml");
       break;
