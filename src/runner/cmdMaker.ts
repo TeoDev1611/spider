704e3b79157e3a899db4a704efa50b80ae5056b7
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-01-05 09:03:16 -0500

    feat: improve the cmd maker function

---
+++ src/runner/cmdMaker.ts
@@ -0,0 +1,125 @@
+import { Choco, Git, Helpers, Scoop } from "runner/types.ts";
+import * as log from "utils/logs.ts";
+
+export function GetCommand(
+  prgm: "scoop" | "git" | "helper" | "choco",
+  action:
+    | "install"
+    | "clone"
+    | "uninstall"
+    | "version"
+    | "clone"
+    | "status"
+    | "cmdAdmin"
+    | "runPs1",
+  flags?: "chocoNightly" | "gitDepth",
+  pkgs?: Array<string> | ["default"],
+  url?: string,
+  dest?: string,
+  file?: string,
+): string {
+  pkgs = (typeof pkgs === "undefined") ? ["cowsay", "echo"] : pkgs;
+  switch (prgm) {
+    case "scoop": {
+      switch (action) {
+        case "install": {
+          return `${Helpers.RunPowershellFile} ${Scoop.Install} ${
+            pkgs.join(" ")
+          }`;
+        }
+
+        case "status": {
+          return `${Helpers.RunPowershellFile} ${Scoop.Status}`;
+        }
+
+        case "uninstall": {
+          return `${Helpers.RunPowershellFile} ${Scoop.Uninstall} ${
+            pkgs.join(" ")
+          }`;
+        }
+
+        case "version": {
+          return `${Helpers.RunPowershellFile} ${Scoop.Version}`;
+        }
+        default: {
+          log.error("Action for scoop not found!");
+          break;
+        }
+      }
+      break;
+    }
+
+    case "git": {
+      url = (typeof url === "undefined")
+        ? "https://github.com/TeoDev1611/astro.nvim"
+        : url;
+      dest = (typeof dest === "undefined") ? "$HOME" : dest;
+      switch (action) {
+        case "clone": {
+          if (flags == "gitDepth") {
+            return `${Git.Clone} ${Git.Flags.DepthFast} ${url} ${dest}`;
+          }
+          return `${Git.Clone} ${url} ${dest}`;
+        }
+
+        case "version": {
+          return `${Git.Version}`;
+        }
+        default:
+          log.error("Action for git not found!");
+          break;
+      }
+      break;
+    }
+
+    case "choco": {
+      switch (action) {
+        case "install": {
+          if (flags === "chocoNightly") {
+            return `${Choco.Install} ${
+              pkgs.join(" ")
+            } ${Choco.Flags.InstallNow} ${Choco.Flags.Nightly}`;
+          }
+          return `${Choco.Install} ${pkgs.join(" ")} ${Choco.Flags.InstallNow}`;
+        }
+
+        case "uninstall": {
+          return `${Choco.Uninstall} ${
+            pkgs.join(" ")
+          } ${Choco.Flags.InstallNow}`;
+        }
+
+        case "version": {
+          return Choco.Version;
+        }
+
+        default:
+          log.error("Action for choco not found!");
+          break;
+      }
+      break;
+    }
+
+    case "helper": {
+      file = (typeof file === "undefined") ? "scoop.ps1" : file;
+      switch (action) {
+        case "cmdAdmin": {
+          return Helpers.CmdAdmin;
+        }
+        case "runPs1": {
+          return `${Helpers.RunPowershellFile} ${file}`;
+        }
+
+        default:
+          log.error("Action not found");
+          break;
+      }
+      break;
+    }
+
+    default:
+      log.error("Program not found!");
+      break;
+  }
+  return " ";
+}
--- src/runner/types.ts
+++ src/runner/types.ts
@@ -1,15 +1,30 @@
 // Setup the windows commands
 export const Scoop = {
-  Status: "powershell -NoLogo -NoProfile scoop status",
-  Install: "powershell -NoLogo -NoProfile scoop install",
-  Version: "powershell -NoLogo -NoProfile scoop version",
+  Status: "scoop status",
+  Install: "scoop install",
+  Version: "scoop version",
+  Uninstall: "scoop uninstall",
 };
 
 export const Git = {
-  Clone: "git clone --depth=1",
+  Clone: "git clone",
   Version: "git --version",
+  Flags: {
+    DepthFast: "--depth=1",
+  },
 };
 
 export const Choco = {
   Version: "choco -v",
+  Install: "choco install",
+  Uninstall: "choco uninstall",
+  Flags: {
+    InstallNow: "-y",
+    Nightly: "--pre",
+  },
+};
+
+export const Helpers = {
+  CmdAdmin: 'powershell -Command "Start-Process cmd -Verb RunAs"',
+  RunPowershellFile: "powershell -NoLogo -NoProfile",
 };
